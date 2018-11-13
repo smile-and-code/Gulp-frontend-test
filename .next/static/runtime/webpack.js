1c9ec1a0c7108ca90307d641a246bd8fb778ebff
Sam Alvarez <smile.and.code@gmail.com>
2018-11-12 20:48:57 -0500

    Themes and Layout with Styled Components

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1f44cccce6df874a34eb";
+/******/ 	var hotCurrentHash = "0f724682b8485ebd8931";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
